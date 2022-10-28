31b15f5d73a3f484079745a9b71cfdc3f4f139c0
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-10-28 13:24:41 +0530

    😁

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   cleanUrls: "without-subfolders",
 
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href="/docs/favicon/apple-touch-icon.png"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/docs/favicon/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/docs/favicon/favicon-32x32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/docs/favicon/favicon-16x16.png"}],
     ['link', { rel: "manifest", href: "/docs/favicon/site.webmanifest"}],
