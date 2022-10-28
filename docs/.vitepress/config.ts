3ec22ce5271ae767bbeeb72c49fa94f4788290a3
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-10-28 13:17:00 +0530

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   cleanUrls: "without-subfolders",
 
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href="/docs/favicon/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/docs/favicon/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/docs/favicon/favicon-16x16.png"}],
+    ['link', { rel: "apple-touch-icon", sizes="180x180", href="/docs/favicon/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes="32x32", href: "/docs/favicon/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes="16x16", href: "/docs/favicon/favicon-16x16.png"}],
     ['link', { rel: "manifest", href: "/docs/favicon/site.webmanifest"}],
     ['link', { rel: "mask-icon", href: "/docs/favicon/safari-pinned-tab.svg", color: "#5bbad5"}],
     ['link', { rel: "shortcut icon", href: "/docs/favicon/favicon.ico"}],
