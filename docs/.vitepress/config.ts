75ff84b70e650a5bb6b02e0f52de5003b4e031f2
Ved Prakash <87353286+VedxP@users.noreply.github.com>
2022-11-18 12:33:47 +0530

    Formatted Analytics Code

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,7 +20,12 @@ export default defineConfig({
     ['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
     ['meta', { name: "theme-color", content: "#ffffff"}],
     ['script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-ZLRHJBGCPE', async: 'async' }],
-    ['script', {}, `  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-ZLRHJBGCPE');`]
+    ['script', {}, ` 
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-ZLRHJBGCPE');`]
   ],
 
   themeConfig: { 
